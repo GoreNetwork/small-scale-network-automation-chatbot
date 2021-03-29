fef5caa0d71f624adad3f6b1a3314993c253dbcf
GoreNetwork <dhimes@gmail.com>
2021-03-29 12:57:04 -0400

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,5 +1,5 @@
 def get_himes_house():
     creds = {}
-    creds['token'] = 'xoxb-887994720226-1337489609012-Fq0X8QKjGOckKypJmi4wYmJK'
+    creds['token'] = 'xoxb-FAKE'
     creds['channel'] = 'C01QKFD8NSH'
     return creds
